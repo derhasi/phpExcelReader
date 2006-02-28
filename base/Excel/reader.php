2cc9ae8ad00e0e3d8546f82f7f0054ca439b1348
trex005 <trex005@05c125d7-b90d-0410-924c-9f63b261d7bc>
2006-02-28 04:25:10 +0000

    Patch submitted by  Roberto Innocenti - robyinno Tested and applied by Trex005

--- base/Excel/reader.php
+++ base/Excel/reader.php
@@ -802,10 +802,15 @@ class Spreadsheet_Excel_Reader {
         return $result;
     }
 
-    function _GetInt4d($data, $pos) {
-        return ord($data[$pos]) | (ord($data[$pos+1]) << 8) | (ord($data[$pos+2]) << 16) | (ord($data[$pos+3]) << 24);
-    }
-
+    function _GetInt4d($data, $pos)
+	{
+		$value = ord($data[$pos]) | (ord($data[$pos+1]) << 8) | (ord($data[$pos+2]) << 16) | (ord($data[$pos+3]) << 24);
+		if ($value>=4294967294)
+		{
+			$value=-2;
+		}
+		return $value;
+	}
 
 }
 
